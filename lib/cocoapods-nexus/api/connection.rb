263a769ee090d0c3f27bcbdaa3aaec1cdf74792d
代岭池 <dailc@asiainfo.com>
2020-09-10 16:55:15 +0800

    update nexus url.

--- lib/cocoapods-nexus/api/connection.rb
+++ lib/cocoapods-nexus/api/connection.rb
@@ -112,7 +112,7 @@ module CocoapodsNexus
 
       def send_request(connection_method, endpoint, parameters: '', headers: {}, api_version: 'v1')
         parameters = parameters.to_json if headers['Content-Type'] == 'application/json'
-        url = "#{@hostname}/nexus/service/rest/#{api_version}/#{endpoint}"
+        url = File.join(@hostname,"/nexus/service/rest/#{api_version}/#{endpoint}")
         catch_connection_error do
           RestClient::Request.execute(
               method: connection_method,
